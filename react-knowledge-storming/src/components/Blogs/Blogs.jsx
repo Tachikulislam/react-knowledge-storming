c90f3f3b7733af692e39ae3e475613d36f8a44ee
Tachikulislam <mdtachikulislam@gmail.com>
2024-11-26 03:34:23 +0600

    Initial commit

---
+++ react-knowledge-storming/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                setBlogs(data);
+            })
+            
+    }, [])
+
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default Blogs;
